c4f21777abdaf8532c7a7c66c53253f56fc79470
xiezixuan <xiezixuan@xiezixuan-PC>
2016-04-25 17:44:51 +0800

    修改

--- src/testt/RedisServer.java
+++ src/testt/RedisServer.java
@@ -1,45 +1,41 @@
 package testt;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class RedisServer {
 
+	int a = 1;
 
-	public void redisPool(){
+	public void redisPool() {
 		System.out.print("begin");
-		
+
 		System.out.print("getPool");
-		
+
 		System.out.print("end");
 	}
-	
-	public void set(){
+
+	public void set() {
 		System.out.print("begin");
 
 		System.out.print("set 1");
-		
+
 		System.out.print("end");
 	}
 
-	
-	public void get(){
-		RedisServers.comPool(new IRedis(){
-			public  void  fun(){
+	public void get() {
+		RedisServers.comPool(new IRedis() {
+			public void fun() {
 				System.out.print("get 1");
 			}
-			
+
 		});
 	}
 
 }
 
-class RedisServers{
-	public static void comPool(IRedis rrss){
+class RedisServers {
+	public static void comPool(IRedis rrss) {
 		System.out.print("begin");
 		rrss.fun();
 		System.out.print("end");
 	}
 }
-
-
-
